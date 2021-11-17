a96e99fbf4fb93e5d8f5927afdc795aff735e62f
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-17 15:54:08 -0600

    first commit

---
+++ shopping.js
@@ -0,0 +1,8 @@
+const shoppingList = [
+    {id: 001, name: `milk`, price: 2},
+    {id: 002, name: `eggs`, price: 1},
+    {id: 003, name: `butter`, price: 3},
+    {id: 004, name: `ground meat`, price: 7}
+]
+
+console.log(shoppingList)
